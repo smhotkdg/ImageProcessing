6193023e68fa21cfc4d06efc33e04fa9e55faaab
zzz123 <cozy0614@gmail.com>
2026-09-21 11:51:05 +0900

    그레이 변환

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import cv2
 
-img = cv2.imread('road.jpg')
-if img is None:
-    #여기 이미지 없는 경우 예외처리 해줘야함
-    raise FileNotFoundError('road.jpg')
-print(img.shape, img.dtype)
-cv2.imshow('IMG', img)
+color = cv2.imread('road.jpg')
+gray = cv2.imread('road.jpg', cv2.IMREAD_GRAYSCALE)
+
+cv2.imshow('COLOR', color)
+cv2.imshow('GRAY', gray)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
