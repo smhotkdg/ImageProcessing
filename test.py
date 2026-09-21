7ab3043f4620925121d25d2f8221078f42d95a3f
zzz123 <cozy0614@gmail.com>
2026-09-21 11:26:28 +0900

    이미지 로드 추가

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
 import cv2
 
 img = cv2.imread('road.jpg')
-
+if img is None:
+    #여기 이미지 없는 경우 예외처리 해줘야함
+    raise FileNotFoundError('road.jpg')
+print(img.shape, img.dtype)
+cv2.imshow('IMG', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
