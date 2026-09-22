d1d1584fc1fdb8b17f2db0e04c1fc6b6a90989a5
seugnhun <cozy0614@gmail.com>
2026-09-22 16:06:27 +0900

    test2 수정

--- test2.py
+++ test2.py
@@ -1,1 +1,14 @@
 import cv2
+
+
+path = str('road.jpg')
+color = cv2.imread(path)
+gray = cv2.cvtColor(color, cv2.COLOR_BGR2GRAY)
+edge = cv2.Canny(gray, 100, 200)
+if color is None or gray is None:
+    raise FileNotFoundError(path)
+print(color.shape, gray.shape)
+cv2.imshow('COLOR', color)
+cv2.imshow('GRAY', gray)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
