e44e6df5a87c7fcc8a38f6ff02dc525038c375e0
zzz123 <cozy0614@gmail.com>
2026-09-23 10:56:20 +0900

    채널 확인 추가

--- test.py
+++ test.py
@@ -9,6 +9,10 @@ saveResult = cv2.imwrite(grayPath, gray)
 if saveResult:
     print('Image saved successfully.')
     gray2 = cv2.imread(grayPath)
+    #gray2 = cv2.imread(grayPath,cv2.IMREAD_GRAYSCALE)
+    print('shape:', gray2.shape)
+    print('gray shape:', gray.shape)
+
     cv2.imshow('GRAY', gray2)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
