50bb841f449d2ebd9e1c4f42eba140acc27c26a5
zzz123 <cozy0614@gmail.com>
2026-09-23 10:44:00 +0900

    흑백처리 및 저장 구현

--- test.py
+++ test.py
@@ -1,10 +1,17 @@
 import cv2
 
 color = cv2.imread('road.jpg')
-gray = cv2.imread('road.jpg', cv2.IMREAD_GRAYSCALE)
+gray = cv2.cvtColor(color, cv2.COLOR_BGR2GRAY)
 
-cv2.imshow('COLOR', color)
-cv2.imshow('GRAY', gray)
+grayPath = 'road_gray.jpg'
+saveResult = cv2.imwrite(grayPath, gray)
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+if saveResult:
+    print('Image saved successfully.')
+    gray2 = cv2.imread(grayPath)
+    cv2.imshow('GRAY', gray2)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+else:
+    print('Failed to save image.')
+
